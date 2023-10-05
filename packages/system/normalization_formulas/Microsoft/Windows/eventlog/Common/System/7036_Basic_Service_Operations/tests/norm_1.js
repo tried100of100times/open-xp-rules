0506377040c554b95aca3f396a8d9f54e75112c4
Andrey <43377995+driverenok@users.noreply.github.com>
2023-10-06 01:57:58 +0300

    Детект SharpNoPSExec (#354)
    * Rule SharpNoPSExec created and tested
    
    * added new normalizations and fixed tests
    
    ---------
    
    Co-authored-by: Anton Kutepov <aw350m3@yandex.com>

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/7036_Basic_Service_Operations/tests/norm_1.js
@@ -0,0 +1,19 @@
+{
+  "action": "stop",
+  "object": "application",
+  "status": "success",
+  "category.generic": "Application",
+  "category.high": "Availability Management",
+  "category.low": "Control",
+  "event_src.category": "Operating system",
+  "event_src.fqdn": "someone.acme.com",
+  "event_src.hostname": "someone",
+  "event_src.subsys": "System",
+  "event_src.title": "windows",
+  "event_src.vendor": "microsoft",
+  "id": "PT_Microsoft_Windows_eventlog_7036_Basic_Service_Operations",
+  "importance": "info",
+  "msgid": "7036",
+  "object.name": "Adobe Flash Player Update Service",
+  "time": "2014-07-30T02:05:00Z"
+}
