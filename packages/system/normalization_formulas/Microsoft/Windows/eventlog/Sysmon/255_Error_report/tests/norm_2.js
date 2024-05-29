195a79efdd082634c0bb6ba14343672306e8b563
Andru <43377995+driverenok@users.noreply.github.com>
2024-05-29 07:58:50 +0300

    Added norm EventID 255 for Microsoft-Windows-Sysmon provider (#362)
    * Added norm EventID 255 for Microsoft-Windows-Sysmon provider
    
    * Updated norm EventID 255 for Microsoft-Windows-Sysmon provider
    
    * Updated norm EventID 255 for Microsoft-Windows-Sysmon provider
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Sysmon/255_Error_report/i18n/i18n_en.yaml
    
    Co-authored-by: zatrahali <41584728+zatrahali@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Sysmon/255_Error_report/i18n/i18n_ru.yaml
    
    Co-authored-by: zatrahali <41584728+zatrahali@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Sysmon/255_Error_report/formula.xp
    
    Co-authored-by: zatrahali <41584728+zatrahali@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    Co-authored-by: zatrahali <41584728+zatrahali@users.noreply.github.com>

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Sysmon/255_Error_report/tests/norm_2.js
@@ -0,0 +1,17 @@
+{
+    "action": "alert",
+    "event_src.category": "Other",
+    "event_src.fqdn": "win10-work.stand2008.local",
+    "event_src.hostname": "win10-work",
+    "event_src.subsys": "Microsoft-Windows-Sysmon/Operational",
+    "event_src.title": "sysmon",
+    "event_src.vendor": "microsoft",
+    "id": "Microsoft_Windows_eventlog_Sysmon_255_Error_report",
+    "importance": "medium",
+    "msgid": "255",
+    "object": "application",
+    "object.name": "DriverCommunication",
+    "reason": "Failed to retrieve events - Last error: Параметр задан неверно.",
+    "status": "success",
+    "time": "2024-04-21T18:52:29.286Z"
+}
