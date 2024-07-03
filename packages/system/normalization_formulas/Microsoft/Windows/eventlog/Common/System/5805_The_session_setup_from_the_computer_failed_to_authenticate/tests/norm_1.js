99b48c9b6f8655e14c2399c1c02fe56e37b08681
Andru <43377995+driverenok@users.noreply.github.com>
2024-07-03 12:52:22 +0300

    Детект Zerologon на основе событий 5805 и 4742 (#399)
    * Added 5805_The_session_setup_from_the_computer_failed_to_authenticate norm
    
    * Updateded 5805_The_session_setup_from_the_computer_failed_to_authenticate norm
    
    * Updated 5805_The_session_setup_from_the_computer_failed_to_authenticate norm
    
    * Updated 5805_The_session_setup_from_the_computer_failed_to_authenticate norm
    
    * Updated CVE_2020_1472_Zerologon_Exploitation detection rule
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/5805_The_session_setup_from_the_computer_failed_to_authenticate/metainfo.yaml
    
    Co-authored-by: Anton Kutepov <61383585+aw350m33d@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/5805_The_session_setup_from_the_computer_failed_to_authenticate/i18n/i18n_en.yaml
    
    Co-authored-by: Anton Kutepov <61383585+aw350m33d@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    * Update packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/5805_The_session_setup_from_the_computer_failed_to_authenticate/i18n/i18n_ru.yaml
    
    Co-authored-by: Anton Kutepov <61383585+aw350m33d@users.noreply.github.com>
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    
    * Updated CVE_2020_1472_Zerologon_Exploitation rule
    
    ---------
    
    Signed-off-by: Andru <43377995+driverenok@users.noreply.github.com>
    Co-authored-by: Anton Kutepov <61383585+aw350m33d@users.noreply.github.com>

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/5805_The_session_setup_from_the_computer_failed_to_authenticate/tests/norm_1.js
@@ -0,0 +1,18 @@
+{
+    "action": "login",
+    "event_src.category": "AAA",
+    "event_src.fqdn": "01566s-win16-ir.threebeesco.com",
+    "event_src.hostname": "01566s-win16-ir",
+    "event_src.subsys": "System",
+    "event_src.title": "netlogon",
+    "event_src.vendor": "microsoft",
+    "id": "Microsoft_Windows_eventlog_Common_System_5805_The_session_setup_from_the_computer_failed_to_authenticate",
+    "importance": "medium",
+    "msgid": "5805",
+    "object": "system",
+    "status": "failure",
+    "subject": "account",
+    "subject.account.name": "01566s-win16-ir",
+    "subject.type": "host",
+    "time": "2020-09-15T19:28:49.359Z"
+}
